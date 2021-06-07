a9555777249d281017e0dd3a370f8b7334b52870
trandinhthao2000 <71816109+trandinhthao2000@users.noreply.github.com>
2021-06-07 22:16:28 +0700

    first-commit

---
+++ WebApplication3/WebApplication3/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,20 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models.BUS;
+
+namespace WebApplication3.Controllers
+{
+    public class LoaiSanPhamController : Controller
+    {
+        // GET: LoaiSanPham
+        public ActionResult Index(String id, int page = 1, int pagesize = 3)
+        {
+            var ds = LoaiSanPhamBUS.ChiTiet(id).ToPagedList(page, pagesize);
+            return View(ds);
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Controllers/ShopController.cs
@@ -0,0 +1,96 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models.BUS;
+using System.Xml.Linq;
+
+namespace WebApplication3.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index(int page = 1, int pagesize = 3)
+        {
+            var db = ShopOnlineBUS.DanhSach().ToPagedList(page, pagesize);
+            return View(db);
+        }
+
+        // GET: Shop/Details/5
+        public ActionResult Details(String id, int page = 1, int pagesize = 3)
+        {
+            var db = ShopOnlineBUS.ChiTiet(id);
+            ViewBag.page = page;
+            ViewBag.pagesize = pagesize;
+            return View(db);
+        }
+
+        // GET: Shop/Create 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shop/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shop/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Shop/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shop/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Shop/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
